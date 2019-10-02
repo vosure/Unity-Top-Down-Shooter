26896297e356a57f6302bd36214d63caafa7aff6
vosure <vosure1@gmail.com>
2019-10-02 23:17:22 +0300

    obstacle placement

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class MapGenerator : MonoBehaviour
 {
     public Transform tilePrefab;
+    public Transform obstaclePrefab;
     public Vector2 mapSize;
 
     [Range(0, 1)]
     public float oulinePercent;
+
+    List<Coordinate> tileCoords;
+    Queue<Coordinate> shuffledTileCoords;
+
+    public int seed = 10;
 
     private void Start()
     {
@@ -17,6 +23,16 @@ public class MapGenerator : MonoBehaviour
 
     public void GenerateMap()
     {
+        tileCoords = new List<Coordinate>();
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                tileCoords.Add(new Coordinate(x, y));
+            }
+        }
+        shuffledTileCoords = new Queue<Coordinate>(Utility.ShuffleArray(tileCoords.ToArray(), seed));
+
         string holderName = "Generated Map";
         if (transform.FindChild(holderName))
         {
@@ -30,11 +46,45 @@ public class MapGenerator : MonoBehaviour
         {
             for (int y = 0; y < mapSize.y; y++)
             {
-                Vector3 tilePosition = new Vector3(-mapSize.x / 2 + 0.5f + x, 0, -mapSize.y / 2 + 0.5f + y);
+                Vector3 tilePosition = CoordToPosition(x, y);
                 Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90)) as Transform;
                 newTile.localScale = Vector3.one * (1 - oulinePercent);
                 newTile.parent = mapHolder;
             }
+        }
+
+        int obstacleCount = 10;
+        for (int i = 0; i < obstacleCount; i++)
+        {
+            Coordinate randomCoord = GetRandomCoordinate();
+            Vector3 obstaclePosition = CoordToPosition(randomCoord.x, randomCoord.y);
+            Transform obstacle = Instantiate(obstaclePrefab, obstaclePosition + Vector3.up * 0.5f, Quaternion.identity) as Transform;
+            obstacle.parent = mapHolder;
+        }
+    }
+
+    Vector3 CoordToPosition(int x, int y)
+    {
+        return new Vector3(-mapSize.x / 2 + 0.5f + x, 0, -mapSize.y / 2 + 0.5f + y);
+    }
+
+    public Coordinate GetRandomCoordinate()
+    {
+        Coordinate randomCoord = shuffledTileCoords.Dequeue();
+        shuffledTileCoords.Enqueue(randomCoord);
+        return randomCoord;
+    }
+
+
+    public struct Coordinate
+    {
+        public int x;
+        public int y;
+
+        public Coordinate(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
         }
     }
 }
---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+public class Utility
+{
+    public static T[] ShuffleArray<T>(T[] array, int seed)
+    {
+        System.Random randomizer = new System.Random(seed);
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            int random = randomizer.Next(i, array.Length);
+            T temp = array[random];
+            array[random] = array[i];
+            array[i] = temp;
+        }
+
+        return array;
+    }
+
+}
